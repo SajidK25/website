8a18fc8e59c52ec998f82a8c6c078ef2d55c0199
Sajid Khan <sajidMacPro@Sajids-MacBook-Pro.local>
2016-08-30 01:13:38 +0600

    AdminLTE convert to blade and working so far

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,123 +1,112 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Md Rasel Ahmed
- * Date: 03-Aug-16
- * Time: 11:00 AM
- */
-?>
-
-        <!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="Md Rasel Ahmed">
-    <title>
-        Project Title - @yield('title')
-    </title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    @yield('head_style')
-    @yield('head_script')
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="{{ url('admin_panel/bootstrap/css/bootstrap.min.css') }}">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ url('admin_panel/dist/css/AdminLTE.min.css') }}">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="{{ url('admin_panel/dist/css/skins/_all-skins.min.css') }}">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="{{ url('admin_panel/plugins/iCheck/flat/blue.css') }}">
+  <!-- Morris chart -->
+  <link rel="stylesheet" href="{{ url('admin_panel/plugins/morris/morris.css') }}">
+  <!-- jvectormap -->
+  <link rel="stylesheet" href="{{ url('admin_panel/plugins/jvectormap/jquery-jvectormap-1.2.2.css') }}">
+  <!-- Date Picker -->
+  <link rel="stylesheet" href="{{ url('admin_panel/plugins/datepicker/datepicker3.css') }}">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="{{ url('admin_panel/plugins/daterangepicker/daterangepicker.css') }}">
+  <!-- bootstrap wysihtml5 - text editor -->
+  <link rel="stylesheet" href="{{ url('admin_panel/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css') }}">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
 </head>
-<!-- ADD THE CLASS layout-boxed TO GET A BOXED LAYOUT -->
-<body class="hold-transition skin-blue layout-boxed sidebar-mini">
-<div class="wrapper">
-    <header class="main-header">        <!-- Logo -->
-        <a href="{{ route('admin::dashboard') }}" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>@yield('app_name_short')</b></span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>@yield('app_name_long')</b></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-            <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                <span class="sr-only">Toggle navigation</span>
-            </a>
-            <div class="navbar-custom-menu">
-                <ul class="nav navbar-nav">
-                    <!-- User Account: style can be found in dropdown.less -->
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="@yield('admin_image')" class="user-image" alt="User Image" style="width: 25px; height: 25px;">
-                            <span class="hidden-xs">@yield('admin_name')</span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <!-- User image -->
-                            <li class="user-header">
-                                <img src="@yield('admin_image')" class="img-circle" alt="User Image" style="width: 90px; height: 90px;">
-                                <p>
-                                    <small>Email: @yield('admin_email')</small>
-                                    <small>Company: @yield('admin_designation')</small>
-                                    <small>Contact: @yield('admin_contact_no')</small>
-                                </p>
-                            </li>
-                            <li class="user-footer">
-                                <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
-                                </div>
-                                <div class="pull-right">
-                                    <a href="{{ url('/logout') }}" class="btn btn-default btn-flat">Sign out</a>
-                                </div>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
-    <!-- Left side column. contains the sidebar -->
-    <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- Sidebar user panel -->
-            <div class="user-panel">
-                <div class="pull-left image">
-                    <img src="@yield('admin_image')" class="img-circle" alt="User Image" style="width: 45px; height: 45px;">
-                </div>
-                <div class="pull-left info">
-                    <p>@yield('admin_name')</p>
-                    <p>@yield('admin_email')</p>
-                </div>
-            </div>
-            <ul class="sidebar-menu">
-                <li class="treeview">
-                    <a href="{{ route('admin::dashboard') }}">
-                        <i class="fa fa-dashboard"></i> <span>Home</span>
-                    </a>
-                </li>
-                @yield('sidebar_menu')
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <h1>
-                @yield('content_header')
-            </h1>
-            <ol class="breadcrumb">
-                @yield('breadcrumb')
-            </ol>
-        </section>
-        <!-- Main content -->
-        <section class="content">
-            @yield('small_box')
-            @yield('page_content')
-        </section><!-- /.content -->
-    </div><!-- /.content-wrapper -->
-    <footer class="main-footer">
-        @yield('footer')
-    </footer>
-    <!-- Add the sidebar's background. This div must be placed immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
-</div><!-- ./wrapper -->
-@yield('body_script')
+<body class="hold-transition skin-blue sidebar-mini">
+  <header class="main-header">
+  <!-- Logo -->
+  <a href="index2.html" class="logo">
+    <!-- mini logo for sidebar mini 50x50 pixels -->
+    <span class="logo-mini"><b>A</b>LT</span>
+    <!-- logo for regular state and mobile devices -->
+    <span class="logo-lg"><b>Admin</b>LTE</span>
+  </a>
+  <!-- Header Navbar: style can be found in header.less -->
+  @include('layouts.partials.nav')
+</header>
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    @include('layouts.partials.sidebar')
+    <!-- /.sidebar -->
+</aside>
+<div class="content-wrapper">
+
+  @yield('content')
+
+</div>
+<!-- /.content-wrapper -->
+  <!-- /.row -->
+
+
+</section>
+<!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+<!--footer-->
+@include('layouts.partials.footer')
+<!--/.footer-->
+<!--footer-->
+<script src="{{ url('admin_panel/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button);
+</script>
+<!-- Bootstrap 3.3.6 -->
+<script src="{{ url('admin_panel/bootstrap/js/bootstrap.min.js') }}"></script>
+<!-- Morris.js charts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="{{ url('admin_panel/plugins/morris/morris.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{ url('admin_panel/plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+<!-- jvectormap -->
+<script src="{{ url('admin_panel/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ url('admin_panel/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- jQuery Knob Chart -->
+<script src="{{ url('admin_panel/plugins/knob/jquery.knob.js') }}"></script>
+<!-- daterangepicker -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
+<script src="{{ url('admin_panel/plugins/daterangepicker/daterangepicker.js') }}"></script>
+<!-- datepicker -->
+<script src="{{ url('admin_panel/plugins/datepicker/bootstrap-datepicker.js') }}"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="{{ url('admin_panel/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') }}"></script>
+<!-- Slimscroll -->
+<script src="{{ url('admin_panel/plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ url('admin_panel/plugins/fastclick/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ url('admin_panel/dist/js/app.min.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="{{ url('admin_panel/dist/js/pages/dashboard.js') }}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{ url('admin_panel/dist/js/demo.js') }}"></script>
 </body>
 </html>
